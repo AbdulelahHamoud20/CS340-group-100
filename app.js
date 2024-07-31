0d5f46ff717576e369e98e48140a09dfe53d251a
murakmei <122371718+murakmei@users.noreply.github.com>
2024-07-30 19:16:37 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+/*
+    SETUP
+*/
+// Express
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+const PORT  = 6301;                 // Set a port number at the top so it's easy to change in the future
+
+// Database
+var db = require('./db-connector')
+
+// Serve static files from the 'public_html' directory
+app.use(express.static(__dirname + '/public_html'));
+
+/*
+    ROUTES
+*/
+// Default route for the homepage
+app.get('/index.html', (req, res) => {
+    res.sendFile(__dirname + '/public_html/index.html');
+});
+
+/*
+    LISTENER
+*/
+app.listen(PORT, () => {
+    console.log(`Express server started on http://localhost:${PORT}; press Ctrl-C to terminate.`);
+});
